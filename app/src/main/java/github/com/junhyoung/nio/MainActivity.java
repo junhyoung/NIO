6c4ede5629ef297c9f9155381b28dcff6dc8f5d9
junhyeong <zz7193@naver.com>
2016-02-04 19:13:28 +0900

    Initial

--- app/src/main/java/github/com/junhyoung/nio/MainActivity.java
+++ app/src/main/java/github/com/junhyoung/nio/MainActivity.java
@@ -1,7 +1,9 @@
 package github.com.junhyoung.nio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    void memo(View v){
+        Intent intent = new Intent(getApplicationContext(),MemoActivity.class);
+        startActivity(intent);
+    }
+    void computer(View v){
+        Intent intent = new Intent(getApplicationContext(),ComputeActivity.class);
+        startActivity(intent);
+    }
+    void calender(View v){
+        Intent intent = new Intent(getApplicationContext(),CalenderActivity.class);
+        startActivity(intent);
+    }
+    void led(View v){
+        Intent intent = new Intent(getApplicationContext(),LedActivity.class);
+        startActivity(intent);
+    }
+    void call(View v){
+        Intent intent = new Intent(getApplicationContext(),ShorteningActivity.class);
+        startActivity(intent);
+    }
+    void map(View v){
+        Intent intent = new Intent(getApplicationContext(),MapActivity.class);
+        startActivity(intent);
     }
 }
