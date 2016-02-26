dcaef6231af0b40d0c7af64786de8f0ca2747874
junhyeong <zz7193@naver.com>
2016-02-26 13:43:26 +0900

    지도, 손전등

--- app/src/main/java/github/com/junhyoung/nio/LedActivity.java
+++ app/src/main/java/github/com/junhyoung/nio/LedActivity.java
@@ -1,13 +1,49 @@
 package github.com.junhyoung.nio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LedActivity extends AppCompatActivity {
+    Camera mCamera = null;
+    ImageView On;
+    ImageView Off;
+
+    @Override
+    protected void onPause() {
+        if (mCamera != null) {
+            mCamera.release();
+        }
+        super.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_led);
+        On=(ImageView)findViewById(R.id.on);
+        Off=(ImageView)findViewById(R.id.off);
+
+
+    }
+
+    public void on(View v) {
+        mCamera = Camera.open();
+        Camera.Parameters mCameraParameter;
+        mCameraParameter = mCamera.getParameters();
+        mCameraParameter.setFlashMode("torch");
+        mCamera.setParameters(mCameraParameter);
+        On.setImageResource(R.drawable.temp);
+        Off.setImageResource(R.drawable.temp);
+    }
+
+    public void off(View v) {
+        if (mCamera != null) {
+            mCamera.release();
+        }
+        On.setImageResource(R.drawable.temp);
+        Off.setImageResource(R.drawable.temp);
     }
 }
--- app/src/main/java/github/com/junhyoung/nio/MapActivity.java
+++ app/src/main/java/github/com/junhyoung/nio/MapActivity.java
@@ -1,13 +1,164 @@
 package github.com.junhyoung.nio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapActivity extends Activity {
+
+    boolean isFirst=true;
+    double lat;
+    double lng;
+    TextView log;
+    TextView add;
+    String address;
+    String curr;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        findlocate();
+        add = (TextView) findViewById(R.id.address);
+        log=(TextView)findViewById(R.id.log);
+        findAddress(lat,lng);
+    }
+    public void copy(View v){
+        add.setText(findAddress(lat, lng));
+        draw(lat, lng);
+
+    }
+
+    public void find(View v) {
+        address = add.getText().toString();
+        Geocoder geocoder = new Geocoder(this);
+        Address addr;
+        LatLng location = null;
+        try {
+            List<Address> listAddress = geocoder.getFromLocationName(address, 1);
+            if (listAddress.size() > 0) { // 주소값이 존재 하면
+                addr = listAddress.get(0); // Address형태로
+                double lat = (double) (addr.getLatitude() * 1E6);
+                double lng = (double) (addr.getLongitude() * 1E6);
+                location = new LatLng(lat, lng);
+
+                Toast.makeText(getApplicationContext(), "LAT :" + lat + "LNG : " + lng, Toast.LENGTH_SHORT).show();
+                Log.d("", "주소로부터 취득한 위도 : " + lat + ", 경도 : " + lng);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        draw(lat, lng);
+    }
+
+    public String findAddress(double lat, double lng) { //위도경도로 주소값 반환
+        String temp="";
+        StringBuffer bf = new StringBuffer();
+        Geocoder geocoder = new Geocoder(this, Locale.KOREA);
+        List<Address> address;
+        String currentLocationAddress;
+        try {
+            if (geocoder != null) {
+                // 세번째 인수는 최대결과값인데 하나만 리턴받도록 설정했다
+                address = geocoder.getFromLocation(lat, lng, 3);
+                // 설정한 데이터로 주소가 리턴된 데이터가 있으면
+                if (address != null && address.size() > 0) {
+                    // 주소
+                    currentLocationAddress = address.get(0).getAddressLine(0).toString();
+
+                    // 전송할 주소 데이터 (위도/경도 포함 편집)
+                    bf.append(currentLocationAddress);
+                    curr=bf.toString();
+                    temp=curr.toString();
+                    bf.append("\n").append("lat:").append(lat).append(", ");
+                    bf.append("lng:").append(lng);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.setText(bf.toString());
+        return temp;
+    }
+
+
+    public void findlocate(){ //위도 경도 구해주는 함수
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // GPS 프로바이더 사용가능여부
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // 네트워크 프로바이더 사용가능여부
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Log.d("Main", "isGPSEnabled=" + isGPSEnabled);
+        Log.d("Main", "isNetworkEnabled=" + isNetworkEnabled);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                lat = location.getLatitude();
+                lng = location.getLongitude();
+
+                findAddress(lat, lng);
+                if(isFirst) {
+                    draw(lat, lng);
+                    isFirst=false;
+                }
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                log.setText("onStatusChanged");
+            }
+
+            public void onProviderEnabled(String provider) {
+                log.setText("onProviderEnabled");
+            }
+
+            public void onProviderDisabled(String provider) {
+                log.setText("onProviderDisabled");
+            }
+        };
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+
+
+    }
+    public void draw(double lat, double lng){
+        LatLng locate=new LatLng(lat,lng);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker marker = map.addMarker(new MarkerOptions().position(locate).title("Here I am!"));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(locate, 15));
+
+        map.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
     }
 }
